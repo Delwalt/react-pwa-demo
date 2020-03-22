1aa68e66c8291859b99306a1a98f738ce0cda3cc
Tarun Khandelwal <Tarun Khandelwal>
2020-03-22 16:07:46 +0530

    Add rumtime cache strategies

--- src/sw-config.js
+++ src/sw-config.js
@@ -8,6 +8,33 @@ if ("function" === typeof importScripts) {
 
     /* injection point for precache manifest file.  */
     workbox.precaching.precacheAndRoute(["_precache-placeholder_"]);
+
+    /* Runtime cache strategies */
+
+    // Cache API response
+    workbox.routing.registerRoute(
+      /^https:\/\/jsonplaceholder\.typicode\.com\/photos/,
+      workbox.strategies.networkFirst({
+        cacheName: "photosApi"
+      })
+    );
+
+    // Api data caching
+    workbox.routing.registerRoute(
+      /^https:\/\/via\.placeholder\.com/,
+      workbox.strategies.cacheFirst({
+        cacheName: "images",
+        plugins: [
+          new workbox.expiration.Plugin({
+            maxEntries: 60,
+            maxAgeSeconds: 30 * 24 * 60 * 60 // 30 Days
+          }),
+          new workbox.cacheableResponse.Plugin({
+            statuses: [0, 200]
+          })
+        ]
+      })
+    );
   } else {
     console.log("Workbox could not be loaded. No Offline support");
   }
